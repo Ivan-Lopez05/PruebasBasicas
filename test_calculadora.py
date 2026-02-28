1eb5d6b09175744797025fcd777f5f7b224120e9
Ivan-Lopez05 <ivan.lopez87@uabc.edu.mx>
2026-02-27 22:31:49 -0800

    Add HTML report template and CSS styles for test results
    - Created a new HTML file (report.html) for displaying test results.
    - Added a CSS file (style.css) to style the report, including layout, colors, and responsive design.
    - Implemented templates for environment information and results table.
    - Included JavaScript for dynamic rendering of test results and filtering options.

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,3 +3,7 @@ from Calculadora import Calculadora
 def test_add():
     calculadora = Calculadora()
     assert calculadora.add(2, 3) == 5
+###Clausula de prueba para la función subtract de la clase Calculadora
+def test_subtract():
+    calculadora = Calculadora()
+    assert calculadora.subtract(5, 2) == 3
