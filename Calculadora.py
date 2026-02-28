34cffc18d14ecc6104d7a47a0f15791d278096a1
Ivan-Lopez05 <ivan.lopez87@uabc.edu.mx>
2026-02-27 22:29:31 -0800

    Add subtraction method to Calculadora and update test documentation

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,6 @@
 class Calculadora:
     def add(self, a, b):
         return a + b
+    ###calculo de la resta
+    def subtract(self, a, b):
+        return a - b
--- test_calculadora.py
+++ test_calculadora.py
@@ -1,5 +1,5 @@
 from Calculadora import Calculadora
-
+###Clausula de prueba para la función add de la clase Calculadora
 def test_add():
     calculadora = Calculadora()
     assert calculadora.add(2, 3) == 5
